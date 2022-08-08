ace2a3739e7d98df2c85c797bef70cecac8ad779
turcocibeira <cibeiragonzalo@gmail.com>
2022-08-08 14:32:42 +0200

    gallery now generates form json file

--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
 const cardDiv = document.querySelector("#cards");
+const galleryDiv = document.querySelector("#galeriaCarousel");
 let btnCons = [];
 const numWsapp = "541151113625"; // Wsapp number goes here 
 
 
 let services = fetch("../json/servicesList.json")
                 .then((response) => response.json())
-                .then(data => createCards(data))
+                .then(data => createCards(data["services"]["data"]["items"]));
+
+let galleryImages = fetch("../json/galleryImages.json")
+                    .then((response) => response.json())
+                    .then(data => createImages(data["images"]));
 
 
 function createCards(data) {
-    let serv = data["services"]["data"]["items"]
-    for (val of serv){
+    for (val of data){
         content = `<div class="card cardServ mx-2 mt-5 border">
                     <img src="../images/${val["image"]}" class="card-img-top" alt="${val["alt"]}">
                     <div class="card-body d-flex flex-column justify-content-between">
@@ -23,11 +27,23 @@ function createCards(data) {
 
         cardDiv.innerHTML += content;
 
-    }
+    };
 
     btnCons = document.querySelectorAll(".consultar");
 
-}
+};
+
+function createImages(data){
+    let isActive = "active";
+    for (val of data){
+        content = `<div class="carousel-item ${isActive}">
+                        <img src="../images/${val["img"]}" class="d-block mx-auto slideImg" alt="${val["alt"]}">
+                    </div>`;
+
+        galleryDiv.innerHTML += content;
+        if (isActive == "active"){isActive = ""};
+    };
+};
 
 function consultaWsapp(producto){
     let wsappLink =  `https://wa.me/${numWsapp}?text=Hola!%20Queria%20consultarte%20sobre%20${producto}`;
